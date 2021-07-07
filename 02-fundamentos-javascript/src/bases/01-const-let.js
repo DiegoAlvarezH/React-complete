323d1b2f02fcbd8d34b7b81cf9c25718598702b2
Diego Alvarez <diegoah905@gmail.com>
2021-07-07 08:38:40 -0500

    empezando con las bases de React

---
+++ 02-fundamentos-javascript/src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+console.log('hola mundo');
+
+const nombre = 'fernando';
+let apellido = 'herrera';
+
+let valorDado = 5;
+valorDado = 4;
+console.log(nombre, apellido, valorDado);
+
+// no se debe de usar var...
+if ( true ) {
+    const nombre = 'peter';
+    let valorDado = 6;
+
+    console.log(valorDado);
+}
