1f41d60fb377e5b0bc5130d7448ea340d9efae1a
Diego Alvarez <diegoah905@gmail.com>
2021-08-02 10:26:51 -0500

    explorando funciones

--- 02-fundamentos-javascript/src/bases/05-funciones.js
+++ 02-fundamentos-javascript/src/bases/05-funciones.js
@@ -1,4 +1,31 @@
 // funciones en JS
-function saludar(nombre) {
+const saludar = function(nombre) {
     return `hola, ${nombre}`;
 }
+
+const saludar2 = (nombre) => {
+    return `Hola, ${nombre}`;
+}
+
+const saludar3 = (nombre) => `Hola, ${nombre}`;
+
+// saludar = 30;
+console.log(saludar('Goku'));
+console.log(saludar2('Vegeta'));
+console.log(saludar3('Gohan'));
+
+const getUser = () => ({
+    uid: 'A1223123312',
+    username: 'Dieho'
+});
+
+const user = getUser();
+console.log(user);
+
+const getUsuarioActivo = (nombre) => ({
+    uid: '4243234234',
+    username: nombre
+});
+
+const usuarioActivo = getUsuarioActivo('Alejandro');
+console.log(usuarioActivo);
