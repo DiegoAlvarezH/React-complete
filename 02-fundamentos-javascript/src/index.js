083f7f3abe7e5c5e1ce6a87bdf6baffe655b420a
Diego Alvarez <diegoah905@gmail.com>
2021-08-04 10:07:39 -0500

    desestructuracion

--- 02-fundamentos-javascript/src/index.js
+++ 02-fundamentos-javascript/src/index.js
@@ -1,31 +1,11 @@
-// funciones en JS
-const saludar = function(nombre) {
-    return `hola, ${nombre}`;
-}
+//Desestructuracion
+//asignacion desestructurante 
+const persona = {
+    nombre: 'Tony',
+    edad: 45,
+    clave: 'Ironman'
 
-const saludar2 = (nombre) => {
-    return `Hola, ${nombre}`;
-}
+};
 
-const saludar3 = (nombre) => `Hola, ${nombre}`;
-
-// saludar = 30;
-console.log(saludar('Goku'));
-console.log(saludar2('Vegeta'));
-console.log(saludar3('Gohan'));
-
-const getUser = () => ({
-    uid: 'A1223123312',
-    username: 'Dieho'
-});
-
-const user = getUser();
-console.log(user);
-
-const getUsuarioActivo = (nombre) => ({
-    uid: '4243234234',
-    username: nombre
-});
-
-const usuarioActivo = getUsuarioActivo('Alejandro');
-console.log(usuarioActivo);
+console.log(persona.nombre);
+console.log(persona.edad);
