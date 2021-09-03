dcc8931053637ccc38924e6e420bc35023255723
Diego Alvarez <diegoah905@gmail.com>
2021-09-02 23:59:33 -0500

    impresion de variables HTML

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -5,15 +5,19 @@ import React from 'react'
 // componentes por funciones - functional components (hooks)
 
 const PrimeraApp = () => {
+    //los booleanos no se imprimen 
+    const saludo = 'hola mundo';
+
 
     return ( 
         //fragment
         //clases
         <>
-        <h1> Hola Mundo</h1>
+        <h1> { saludo }</h1>
         <p>mi primera aplicacion</p>
         </>
     );
+    
 }
 
 export default PrimeraApp;
